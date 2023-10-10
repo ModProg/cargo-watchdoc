08b4a61fcbf663f1979deed74081f27a0363b033
Roland Fredenhagen <dev@modprog.de>
2023-10-10 20:28:07 +0200

    Fix blocking watchexec queues

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,50 @@
 #![warn(clippy::pedantic)]
-#![allow(clippy::wildcard_imports)]
+#![allow(clippy::wildcard_imports, clippy::too_many_lines)]
 
-use std::io;
 use std::net::{Ipv4Addr, SocketAddrV4};
 use std::path::MAIN_SEPARATOR;
-use std::process::ExitStatus;
 use std::sync::Arc;
 
 use anyhow::Context;
-use axum::{Router, Server};
+use axum::response::Html;
+use axum::{routing, Router, Server};
+use cargo_config2::DocConfig;
 use cargo_metadata::{Metadata, MetadataCommand};
+use clap::Parser;
 use log::{debug, info};
 use tokio::select;
 use tower_http::services::ServeDir;
 use tower_livereload::LiveReloadLayer;
 use watchexec::action::{Action, Outcome};
+use watchexec::command::Command;
 use watchexec::config::{InitConfig, RuntimeConfig};
-use watchexec::handler::PrintDebug;
+use watchexec::event::ProcessEnd;
 use watchexec::Watchexec;
 use watchexec_filterer_globset::GlobsetFilterer;
 
 type Result<T = (), E = anyhow::Error> = std::result::Result<T, E>;
 
-async fn cargo_doc() -> Result<ExitStatus, std::io::Error> {
-    info!("running cargo doc");
-    tokio::process::Command::new("cargo")
-        .arg("doc")
-        // .arg("--no-deps")
-        .status()
-        .await
+#[derive(Parser, Debug)]
+#[command(bin_name = "cargo")]
+enum Cli {
+    Watchdoc {
+        /// Opens docs in webbrowser
+        ///
+        /// If a package is specied it is opened, otherwise the root package is
+        /// selected.
+        #[arg(
+            short, long, num_args = 0..=1,
+            default_missing_value = "crate",
+            value_name = "PACKAGE",
+        )]
+        open: Option<String>,
+        /// Clears terminal between runs
+        #[arg(short, long)]
+        clear: bool,
+        /// Arguments after `--` are passed to `cargo doc`
+        #[arg(allow_hyphen_values = true, last = true)]
+        cargo_doc_args: Vec<String>,
+    },
 }
 
 #[tokio::main]
@@ -37,6 +53,17 @@ async fn main() -> Result {
 }
 
 async fn run() -> Result {
+    let Cli::Watchdoc {
+        open,
+        cargo_doc_args,
+        clear,
+    } = Cli::parse();
+
+    let cargo_config2::Config {
+        doc: DocConfig { browser, .. },
+        ..
+    } = cargo_config2::Config::load()?;
+
     stderrlog::new().init()?;
 
     let ref metadata @ Metadata {
@@ -45,12 +72,22 @@ async fn run() -> Result {
         ..
     } = MetadataCommand::new().exec()?;
 
-    cargo_doc().await?;
+    let command = Command::Exec {
+        prog: "cargo".into(),
+        args: ["doc".into()].into_iter().chain(cargo_doc_args).collect(),
+    };
+    command.to_spawnable()?.status().await?;
 
     let livereload = LiveReloadLayer::new();
     let reloader = livereload.reloader();
     let app = Router::new()
-        .nest_service("/", ServeDir::new(target_directory.join("doc")))
+        .nest_service(
+            "/",
+            ServeDir::new(target_directory.join("doc")).not_found_service(routing::get(|| async {
+                Html(r#"404 <a href="/help.html?search=">Back to Search</a>"#)
+            })),
+        )
+        // .fallback(|| async {"404"})
         .layer(livereload);
 
     let port = if portpicker::is_free(4153) {
@@ -61,15 +98,32 @@ async fn run() -> Result {
     let addr = &SocketAddrV4::new(Ipv4Addr::LOCALHOST, port).into();
     let app = Server::bind(addr).serve(app.into_make_service());
 
-    // TODO select either workspace-root package or alphabetically first
-
-    let root = &metadata
-        .root_package()
-        .or_else(|| metadata.workspace_packages().first().copied())
+    let root = open
+        .as_deref()
+        .and_then(|o| (o != "crate").then_some(o))
+        .or_else(|| {
+            Some(
+                &metadata
+                    .root_package()
+                    .or_else(|| metadata.workspace_packages().first().copied())?
+                    .name,
+            )
+        })
         .context("Project must have either a root package or workspace members")?
-        .name.replace('-', "_");
+        .replace('-', "_");
 
-    eprintln!("Serving docs at: http://{addr}/{root}");
+    let addr = format!("http://{addr}/{root}");
+    eprintln!("Serving docs at: {addr}");
+    if open.is_some() {
+        if let Some(browser) = browser {
+            std::process::Command::new(browser.path)
+                .args(browser.args)
+                .arg(addr)
+                .spawn()?;
+        } else {
+            opener::open(addr)?;
+        }
+    }
 
     let list = [
         // Mac
@@ -97,13 +151,11 @@ async fn run() -> Result {
 
     debug!("Default ignores: {:?}", list);
 
-    let mut init = InitConfig::default();
-    init.on_error(PrintDebug(std::io::stderr()));
-
     let we = Watchexec::new(
-        init,
+        InitConfig::default(),
         RuntimeConfig::default()
             .pathset([workspace_root])
+            .command(command)
             .filterer(Arc::new(
                 GlobsetFilterer::new(
                     &workspace_root,
@@ -121,13 +173,30 @@ async fn run() -> Result {
             .on_action(move |action: Action| {
                 let reloader = reloader.clone();
                 async move {
-                    debug!("{action:?}");
+                    debug!("Handle actions: {action:?}");
                     if action.events.iter().any(|e| e.signals().next().is_some()) {
                         action.outcome(Outcome::Exit);
-                        Ok::<_, io::Error>(())
+                        Ok::<_, std::convert::Infallible>(())
                     } else {
-                        if cargo_doc().await?.success() {
+                        if action.events.iter().any(|e| {
+                            info!("Reloading Docs");
+                            e.completions()
+                                .any(|c| c.is_some_and(|c| c == ProcessEnd::Success))
+                        }) {
                             reloader.reload();
+                        }
+                        if action.events.iter().any(|e| e.paths().next().is_some()) {
+                            action.outcome(Outcome::both(
+                                Outcome::Wait,
+                                Outcome::both(
+                                    if clear {
+                                        Outcome::Clear
+                                    } else {
+                                        Outcome::DoNothing
+                                    },
+                                    Outcome::Start,
+                                ),
+                            ));
                         }
                         Ok(())
                     }
